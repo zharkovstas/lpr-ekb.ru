52cbcbd9b2ffafa3d6e39a7e0df2c449e29a449f
Stanislav Zharkov <zharkovstas@gmail.com>
2020-05-30 18:31:05 +0500

    fix sw

--- src/static/sw.js
+++ src/static/sw.js
@@ -9,10 +9,26 @@ self.addEventListener('fetch', event => {
 
             event.waitUntil(async function () {
                 const cache = await caches.open('my-cache-name');
-                await cache.put(normalizedUrl, await fetchResponseCloneP);
+                await cache.put(normalizedUrl, await fetchResponseCloneP.then(cleanResponse));
             }());
 
             return (await caches.match(normalizedUrl)) || fetchResponseP;
         }());
     }
 });
+
+function cleanResponse(response) {
+    const clonedResponse = response.clone();
+
+    const bodyPromise = 'body' in clonedResponse ?
+        Promise.resolve(clonedResponse.body) :
+        clonedResponse.blob();
+
+    return bodyPromise.then((body) => {
+        return new Response(body, {
+            headers: clonedResponse.headers,
+            status: clonedResponse.status,
+            statusText: clonedResponse.statusText,
+        });
+    });
+}
