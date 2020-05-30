89e4ef2d6fc62950df5dda8d4507acaf176b2745
Stanislav Zharkov <zharkovstas@gmail.com>
2020-05-30 21:10:03 +0500

    fix sw

--- src/static/sw.js
+++ src/static/sw.js
@@ -1,34 +1,16 @@
 self.addEventListener('fetch', event => {
-    if (event.request.mode === 'navigate') {
+    const requestUrl = new URL(event.request.url);
+    if (event.request.mode === 'navigate' && requestUrl.pathname.endsWith('/')) {
         event.respondWith(async function () {
-            const normalizedUrl = new URL(event.request.url);
-            normalizedUrl.search = '';
-
-            const fetchResponseP = fetch(normalizedUrl);
+            const fetchResponseP = fetch(event.request);
             const fetchResponseCloneP = fetchResponseP.then(r => r.clone());
 
             event.waitUntil(async function () {
                 const cache = await caches.open('my-cache-name');
-                await cache.put(normalizedUrl, await fetchResponseCloneP.then(cleanResponse));
+                await cache.put(requestUrl, await fetchResponseCloneP);
             }());
 
-            return (await caches.match(normalizedUrl)) || fetchResponseP;
+            return (await caches.match(requestUrl)) || fetchResponseP;
         }());
     }
 });
-
-function cleanResponse(response) {
-    const clonedResponse = response.clone();
-
-    const bodyPromise = 'body' in clonedResponse ?
-        Promise.resolve(clonedResponse.body) :
-        clonedResponse.blob();
-
-    return bodyPromise.then((body) => {
-        return new Response(body, {
-            headers: clonedResponse.headers,
-            status: clonedResponse.status,
-            statusText: clonedResponse.statusText,
-        });
-    });
-}
